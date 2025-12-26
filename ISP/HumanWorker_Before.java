10e187f0ceb4a74e99874523c635bc88b6bc680c
le-million28 <f6f5ckqj82@privaterelay.appleid.com>
2025-12-26 14:18:33 +0000

    Create HumanWorker_Before.java

---
+++ ISP/HumanWorker_Before.java
@@ -0,0 +1,8 @@
+class HumanWorker implements Worker {
+    public void work() {
+        System.out.println("Les humains travaillent");
+    }
+    public void eat() {
+        System.out.println("Les humains mangent");
+    }
+}
