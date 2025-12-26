f8ef1b09a34e0ea10552f657eb25c24503f65169
le-million28 <f6f5ckqj82@privaterelay.appleid.com>
2025-12-26 14:21:55 +0000

    Create HumanWorker.java

---
+++ ISP/HumanWorker.java
@@ -0,0 +1,10 @@
+package ict301.solid.isp;
+
+public class HumanWorker implements Eatable {
+    public void work() {
+        System.out.println("Les humains travaillent");
+    }
+    public void eat() {
+        System.out.println("Les humains mangent");
+    }
+}
