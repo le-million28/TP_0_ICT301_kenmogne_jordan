03087bc120d81bcb9f9d51d2ff3cb7e8cc619d27
le-million28 <f6f5ckqj82@privaterelay.appleid.com>
2025-12-26 14:51:26 +0000

    Add files via upload

---
+++ SRP/avant_refactoring.java
@@ -0,0 +1,5 @@
+public class Employe {
+    public void calculerSalaire() { /* Logique de calcul */ }
+    public void sauvegarderDansBaseDonnees() { /* Logique JDBC */ }
+    public void genererRapportPerformance() { /* Logique de formatage */ }
+}
